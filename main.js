6795150b99d9eff97364a7c5c704bad4d3121862
Eanaj8 <Eanaj8@gmail.com>
2019-02-26 20:25:42 -0500

    About Me

---
+++ main.js
@@ -0,0 +1,18 @@
+let name = prompt("What is your name?");
+let color = prompt("What is your favorite color?");
+let address = prompt("Where do you live?");
+let hobbies = prompt("What are your favorite hobbies?");
+let myAge = confirm ("Are you 18 or older?")
+
+
+console.log(name + ' from ' + address + '!');
+
+alert(' Hello ' + name + ' from ' + address + '!');
+
+
+console.log(name);
+console.log(color)
+console.log(address);
+console.log(hobbies);
+console.log(myAge);
+
